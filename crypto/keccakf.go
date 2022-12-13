65ff84119dc8f68261e749dc60307fd543061137
Dustin Ray <dustinray313@gmail.com>
2022-12-13 11:20:01 -0800

    a bunch of things

--- crypto/keccakf.go
+++ crypto/keccakf.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"crypto/rand"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
 var (
 	// The Keccak permutation
 	keccakf func([]uint64)
@@ -12,90 +20,76 @@ var (
 	// number of rounds
 	nrRounds = 24
 )
+var (
+	bc, i, j int
+)
+
+// The round constants
+var rc = [24]uint64{
+	0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
+	0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
+	0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
+	0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
+	0x000000008000808b, 0x800000000000008b, 0x8000000000008089,
+	0x8000000000008003, 0x8000000000008002, 0x8000000000000080,
+	0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
+	0x8000000000008080, 0x0000000080000001, 0x8000000080008008,
+}
+
+var rotc = [24]uint{1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 2, 14,
+	27, 41, 56, 8, 25, 43, 62, 18, 39, 61, 20, 44}
+
+var piln = [24]uint{10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4,
+	15, 23, 19, 13, 12, 2, 20, 14, 22, 9, 6, 1}
 
 // Keccak hashing function
-// Accepts a message in bytes and returns its Keccak hash
-func Keccak(message []byte, inRate int) []byte {
+// Accepts a message in bytes and hashes in place.
+func Keccak(message []byte, inRate int) {
 	// Initialize vars
 	rate = inRate
 	// Initialize the state
 	state = make([]uint64, 25)
 
+	for i := 0; i < 25; i++ {
+		state[i] = 0
+	}
+
 	// The permutation
 	keccakf = func(state []uint64) {
-		var (
-			bc, i, j int
-		)
-		A := make([]uint64, 25)
-		// The round constants
-		rc := [24]uint64{
-			0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
-			0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
-			0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
-			0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
-			0x000000008000808b, 0x800000000000008b, 0x8000000000008089,
-			0x8000000000008003, 0x8000000000008002, 0x8000000000000080,
-			0x000000000000800a, 0x800000008000000a, 0x8000000080008081,
-			0x8000000000008080, 0x0000000080000001, 0x8000000080008008,
-		}
-
-		rotc := [24]uint{1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 2, 14,
-			27, 41, 56, 8, 25, 43, 62, 18, 39, 61, 20, 44}
-
-		piln := [24]uint{10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4,
-			15, 23, 19, 13, 12, 2, 20, 14, 22, 9, 6, 1}
-
 		// The round function
-		round := func() {
+		for bc := 0; bc < nrRounds; bc++ {
 			// Theta step
 			C := make([]uint64, 5)
-			for i = 0; i < 5; i++ {
-				C[i] = A[i] ^ A[i+5] ^ A[i+10] ^ A[i+15] ^ A[i+20]
+			for i := 0; i < 5; i++ {
+				C[i] = state[i] ^ state[i+5] ^ state[i+10] ^ state[i+15] ^ state[i+20]
 			}
-			for i = 0; i < 5; i++ {
+			for i := 0; i < 5; i++ {
 				D := C[(i+4)%5] ^ rotl_64(C[(i+1)%5], 1)
-				for j = 0; j < 25; j += 5 {
-					A[j+i] ^= D
+				for j := 0; j < 25; j += 5 {
+					state[j+i] ^= D
 				}
 			}
 
 			// Rho and pi steps
-			t := A[1]
-			for i = 0; i < 24; i++ {
+			t := state[1]
+			for i := 0; i < 24; i++ {
 				j := piln[i]
-				C[0] = A[j]
-				A[j] = rotl_64(t, rotc[i])
+				C[0] = state[j]
+				state[j] = rotl_64(t, rotc[i])
 				t = C[0]
 			}
 
 			// Chi step
-			for j = 0; j < 25; j++ {
-
+			for j := 0; j < 25; j++ {
 				C := make([]uint64, 5)
-				copy(C, A[5+j-5:])
+				copy(C, state[5+j-5:])
 
-				for i = 0; i < 5; i += 5 {
-					A[j+i] = A[j+i] ^ ((^C[(i+1)%5]) & C[(i+2)%5])
+				for i := 0; i < 5; i += 5 {
+					state[j+i] = state[j+i] ^ ((^C[(i+1)%5]) & C[(i+2)%5])
 				}
 			}
-
 			// Iota step
-			A[0] ^= rc[bc]
-		}
-
-		// Copy the state into A, B, C and D
-		for i = 0; i < 25; i++ {
-			A[i] = state[i]
-		}
-
-		// Perform the rounds
-		for bc = 0; bc < nrRounds; bc++ {
-			round()
-		}
-
-		// Copy the state back
-		for i = 0; i < 25; i++ {
-			state[i] = A[i]
+			state[0] ^= rc[bc]
 		}
 	}
 
@@ -112,19 +106,19 @@ func Keccak(message []byte, inRate int) []byte {
 		keccakf(state)
 	}
 
-	// Squeeze the output
-	output := make([]byte, outputLength)
-	for i := 0; i < len(output); i += 8 {
-		block := state[i/8 : i/8+8]
-		for j := 0; j < len(block); j++ {
-			output[i+j] = byte(block[j] >> (uint(j) * 8))
-		}
-		if i+8 >= outputLength {
-			break
-		}
-		keccakf(state)
-	}
-	return output
+	// // Squeeze the output
+	// output := make([]byte, outputLength)
+	// for i := 0; i < len(output); i += 8 {
+	// 	block := state[i/8 : i/8+8]
+	// 	for j := 0; j < len(block); j++ {
+	// 		output[i+j] = byte(block[j] >> (uint(j) * 8))
+	// 	}
+	// 	if i+8 >= outputLength {
+	// 		break
+	// 	}
+	// 	keccakf(state)
+	// }
+	// return output
 }
 
 // Rotate left
@@ -190,17 +184,64 @@ func bytepad(X []byte, w uint64) []byte {
 
 }
 
-// func main() {
-// 	b := generateRandomBytes()
-// 	fmt.Println(Keccak(b), 1088)
-// }
+func readFileChunk(file *os.File, chunkSize int64, pos int64, wg *sync.WaitGroup, chunks *chan []byte) {
+	b := make([]byte, chunkSize)
+	file.ReadAt(b, pos*chunkSize)
+	Keccak(b, 1088)
+	wg.Done()
+	*chunks <- b
+}
 
-// func generateRandomBytes() []byte {
-// 	b := make([]byte, 1000)
-// 	_, err := rand.Read(b)
-// 	if err != nil {
-// 		fmt.Println("error:", err)
-// 		return nil
-// 	}
-// 	return b
-// }
+// func KeccakChunk(chunks *chan []byte)
+
+func run() {
+
+	start := time.Now() // start time
+	fileName := "/home/dr/Downloads/bible.txt"
+	// fileName := "/home/dr/Downloads/input.file"
+
+	var wg sync.WaitGroup
+
+	fileInfo, err := os.Stat(fileName)
+	if err != nil {
+		panic(err)
+	}
+
+	fileSize := fileInfo.Size()
+	threads := int64(fileSize / 64)
+	chunkSize := int64(fileSize / threads)
+
+	chunks := make(chan []byte)
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	for i := 0; int64(i) < threads; i++ {
+		wg.Add(1)
+		go readFileChunk(f, chunkSize, int64(i), &wg, &chunks)
+	}
+
+	wg.Wait()
+	end := time.Since(start)
+	fmt.Println("Done reading file.")
+	fmt.Println("Time elapsed: ", end)
+}
+
+func main() {
+
+	run()
+
+}
+
+func generateRandomBytes() []byte {
+	b := make([]byte, 1000)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("error:", err)
+		return nil
+	}
+	return b
+}
