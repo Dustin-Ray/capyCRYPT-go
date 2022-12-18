c8ce4ba6f0e79023fab9ba8bfd634a7d32f0603e
Dustin Ray <dustinvonsandwich@gmail.com>
2022-12-17 16:21:21 -0800

    fileops

--- fileReader.go
+++ fileReader.go
@@ -4,22 +4,21 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"time"
 )
 
-const producerCount int = 64
-const consumerCount int = 64
+const producerCount int = 4
+const consumerCount int = 4
 
 func produce(file *os.File, pos int64, link chan []byte, wg *sync.WaitGroup) {
 	// Open the file
-	f, err := os.Open("bible.txt")
+	f, err := os.Open("/home/dr/Downloads/movie/movie.mp4")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
-
 	// Create a buffer to read the file into
 	buf := make([]byte, 8192)
-
 	// Read the file into the buffer
 	_, err = f.ReadAt(buf, pos)
 	if err != nil {
@@ -27,29 +26,29 @@ func produce(file *os.File, pos int64, link chan []byte, wg *sync.WaitGroup) {
 	}
 	// Push the buffer into the channel
 	link <- buf
-	fmt.Println(pos)
 	wg.Done()
 }
 
 func consume(link <-chan []byte, wg *sync.WaitGroup) {
 	defer wg.Done()
-	ch := <-link
-	SpongeSqueeze(SpongeAbsorb(&ch, 512), 1344/8, 512)
-
+	for ch := range link {
+		ch = <-link
+		SpongeSqueeze(SpongeAbsorb(&ch, 512), 1344/8, 512)
+	}
 }
 
 func main() {
 
-	file, _ := os.Open("bible.txt")
+	file, _ := os.Open("/home/dr/Downloads/movie/movie.mp4")
 	fileInfo, _ := file.Stat()
 	fileSize := fileInfo.Size()
 	chunkSize := 8192
-	chunks := fileSize / int64(chunkSize)
-
+	chunks := fileSize/int64(chunkSize) - 2
+	fmt.Println("number of goroutines: ", chunks)
 	link := make(chan []byte)
 	wp := &sync.WaitGroup{}
 	wc := &sync.WaitGroup{}
-	fmt.Println(chunks)
+	start := time.Now()
 	for i := 0; int64(i) < chunks; i++ {
 		wp.Add(1)
 		go produce(file, int64(i*8192), link, wp)
@@ -59,8 +58,9 @@ func main() {
 		wc.Add(1)
 		go consume(link, wc)
 	}
-
 	wp.Wait()
 	close(link)
 	wc.Wait()
+	end := time.Now()
+	fmt.Println("Elapsed time: ", end.Sub(start))
 }
